7a98c334bc6beab89377b97cb750e0d9d309f074
Junaidahmedjs1 <junaidahmed31254@gmail.com>
2024-09-15 17:30:31 +0500

    Chapter 1 to 5 Complete

---
+++ app.js
@@ -0,0 +1,205 @@
+// CHAPTER NO 1 STARTED
+
+// Answer 1
+
+// alert("ThankYou For Coming on my Website")
+
+// Answer 2
+
+// alert("Error! Please enter a valid password")
+
+// Answer 3
+
+// alert(`Welcome to JS Land...
+// Happy Coding`)
+
+// Answer 4 
+
+// alert("Welcome to Js Land")
+// alert("Happy coding")
+
+// Answer 5
+
+// console.log("Hello... I can run Js through my web browser's console ")
+
+// CHAPTER NO 1 ENDED
+
+
+// CHAPTER NO 2 STARTED
+
+// Answer No 1
+
+// var Username ="Junaid Ahmed"
+
+// Answer No 2 
+
+// var myName ="Junaid Ahmed"
+// console.log (myName);
+
+// Answer No 3
+
+// var message = "Hello World"
+// console.log(message);
+// alert(message)
+
+// Answer No 4 
+
+// alert ("My Name is Junaid")
+// alert ("My Age is 20")
+// alert ("I am Marn Stack Developer")
+
+//Answer No 5
+
+// alert (`PIZZA
+// PIZZ
+// PIZ
+// PI
+// P`)
+
+//Answer No 6 
+
+// var email = "Junaidahmed31254@gmail.com"
+// alert(`My Email is ${email}`)
+
+
+//Answer No 7
+
+// var book = "A smarter Way to Learn Javascript"
+// alert(`I'm Trying to learn From the Book ${book}`)
+
+// Answer No 8
+
+// var head = document.querySelector("#head")
+// head.innerHTML = "Yah ! I can write Html content Through Javascript "
+
+
+// Answer No 9 
+
+// alert ("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬")
+
+// CHAPTER NO 2 ENDED
+
+// CHAPTER NO 3 STARTED
+
+// Answer NO 1 
+
+// var age = "My age is 20 Year"
+// alert (age)
+
+// Answer NO 2
+
+
+// var visitor= prompt('vistor visit to the webpage');
+// console.log(`You have visited this site ${visitor} times`);
+
+
+// Answer NO 3
+
+// var birth =2004
+// console.log(`My Birth Year is ${birth} `);
+
+// CHAPTER NO 3 ENDED
+
+
+// CHAPTER NO 4 STARTED
+
+// Answer No 2
+
+// legal variable name
+// var name="Junaid"
+// var Father_Name="Ghulam khuwaja"
+// var id=123;
+// var result;
+// var class_name;
+ 
+// illegal Name of variable;
+// var @name="wali";
+// var 124fathername="";
+// var +class_name="";
+// var name class=""
+// var id()="erere"
+
+// CHAPTER NO 4 ENDED
+
+
+// CHAPTER NO 5 STARTED
+
+
+// Answer No 1
+
+// var number1 = +prompt("Enter Your Number")
+// var number2 = +prompt("Enter Your Number")
+// var result = number1+number2
+// console.log(`The Sum of  ${number1} and ${number2} is  ${result} `);
+
+// Answer No 2
+
+// var number1 = prompt("Enter Your Number")
+// var number2 = prompt("Enter Your Number")
+// var result = number1-number2
+// console.log(`The Subtraction of  ${number1} and ${number2} is  ${result} `);
+
+// var number1 = prompt("Enter Your Number")
+// var number2 = prompt("Enter Your Number")
+// var result = number1*number2
+// console.log(`The Multiplication of  ${number1} and ${number2} is  ${result} `);
+
+
+// var number1 = prompt("Enter Your Number")
+// var number2 = prompt("Enter Your Number")
+// var result = number1/number2
+// console.log(`The Division of  ${number1} and ${number2} is  ${result} `);
+
+
+// Answer No 4
+
+
+// var ticket_price = 600
+// var total_price = 600*5
+// console.log(`Total Cost to buy 5 Tickets is ${total_price} PKR`);
+
+
+// Answer No 5
+
+
+// var number1 = prompt ("Enter Your Number ")
+// var number2 = prompt ("Enter Your Number ")
+// var number3 = prompt ("Enter Your Number ")
+// var number4 = prompt ("Enter Your Number ")
+// var number5 = prompt ("Enter Your Number ")
+// var number6 = prompt ("Enter Your Number ")
+// var number7 = prompt ("Enter Your Number ")
+// var number8 = prompt ("Enter Your Number ")
+// var number9 = prompt ("Enter Your Number ")
+// var number10 = prompt ("Enter Your Number ")
+// var result1 = number1*1;
+// var result2 = number2*2;
+// var result3 = number3*3;
+// var result4 = number4*4;
+// var result5 = number5*5;
+// var result6 = number6*6;
+// var result7 = number7*7;
+// var result8 = number8*8;
+// var result9 = number9*9;
+// var result10 = number10*10;
+// console.log( "4 x 1= "+result1);
+// console.log( "4 x 2= " + result2 );
+// console.log( "4 x 3= " + result3 );
+// console.log( "4 x 4= " + result4 );
+// console.log( "4 x 5= " + result5 );
+// console.log( "4 x 6= " + result6 );
+// console.log( "4 x 7= " + result7 );
+// console.log( "4 x 8= " + result8 );
+// console.log( "4 x 9= " + result9 );
+// console.log( "4 x 10= " + result10); 
+
+
+// CHAPTER NO 5 ENDED
+
+
+
+
+
+
+
+
